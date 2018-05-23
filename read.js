292c1621f3473aa3c340079f97408189c86126dc
Sarumathi <lekha.s91@gmail.com>
2018-05-23 15:47:55 -0700

    Output format

--- read.js
+++ read.js
@@ -1,40 +1,55 @@
-//retrieve task names and their due dates of project: P4 which has some pending tasks
+//Retrieve task names and their due dates of project: P4 which has some pending tasks
 
-result=db.projects.find({project:"P4"},(user.tasks.task_name,user.tasks.due_on));
+result=db.projects.find({project:"P4"},{"user.tasks.task_name":1,"user.tasks.due_on":1,_id:0});
 print('Result for "retrieve task names and their due dates of project: P4 which has some pending tasks"');
 
-// Using the code from the "Write Scripts for the mongo Shell" tutorial from doc.mongodb.org
 while ( result.hasNext() ) {
    printjson( result.next() );
 }
 
-//OUTPUT:
-/*
-task10	August 11, 2018
-task9	Novemer 9, 2018
+
+/* OUTPUT:
+{
+   "user" : [
+		{
+			"tasks" : [
+				{
+					"task_name" : "task10",
+					"due_on" : ISODate("2018-08-11T07:00:00Z")
+				}
+			]
+		},
+      {
+			"tasks" : [
+				{
+					"task_name" : "task9",
+					"due_on" : ISODate("2018-11-09T08:00:00Z")
+				}
+			]
+		},
+
+
 */
 
- //retrieve project names which hs a particular section: QA
+ //retrieve project names which has a particular section: QA
 
-result=db.projects.find({sectionName:"QA"},{project});
+result=db.projects.find({"section.sectionName": "qa"},{"project":1,_id:0});
 print('Result for "retrieve project names which hs a particular section: QA"');
 
-// Using the code from the "Write Scripts for the mongo Shell" tutorial from doc.mongodb.org
 while ( result.hasNext() ) {
    printjson( result.next() );
 } 
 
-// OUTPUT:
-/*
-P1
-P2
-P3
-P4
+/* OUTPUT:
+{ "project" : "P1" }
+{ "project" : "P2" }
+{ "project" : "P3" }
+{ "project" : "P4" }
 */
 
- // retrieve list of tasks of projects under workspace 1
+ // Retrieve list of tasks of projects under workspace 1
 
- result=db.projects.find({workspaceID:1},{user.tasks.task_name});
+ result=db.projects.find({workspaceID:1},{"project":1,"user.tasks.task_name":1,_id:0});
  print('Result for "retrieve list of tasks of projects with under workspace 1"');
 
 // Using the code from the "Write Scripts for the mongo Shell" tutorial from doc.mongodb.org
@@ -42,10 +57,57 @@ while ( result.hasNext() ) {
    printjson( result.next() );
 }
 
-//OUTPUT:
-/*
-task7
-task8
-task3
-task1
+
+/* OUTPUT:
+{
+	"project" : "P1",
+	"user" : [
+   		{
+			"tasks" : [
+				{
+					"task_name" : "task7"
+				}
+			]
+		},
+		{
+			"tasks" : [
+				{
+					"task_name" : "task8"
+				}
+			]
+		},
+		{
+			"tasks" : [
+				{
+					"task_name" : "task3"
+				}
+			]
+		},
+      			"tasks" : [
+				{
+					"task_name" : "task1"
+				}
+			]
+		}
+	]
+}
+{
+	"project" : "P4",
+	"user" : [
+		{
+			"tasks" : [
+				{
+					"task_name" : "task10"
+				}
+			]
+		},
+      		{
+			"tasks" : [
+				{
+					"task_name" : "task9"
+				}
+			]
+		},
+	]
+}
 */
