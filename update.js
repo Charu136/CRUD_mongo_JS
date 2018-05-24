2ec52ca1c54fd8a5968a8409ab4986e685a8db0c
Sarumathi <lekha.s91@gmail.com>
2018-05-23 19:03:56 -0700

    Update update.js

--- update.js
+++ update.js
@@ -4,23 +4,23 @@ db.projects.update({projectID:124,"user.tasks.task_id": 23},
                 {$set: {"user.tasks.$.completed_on": new Date("January 6, 2018 13:06")}},{upsert: true});
 db.projects.update({projectID:124,"user.tasks.task_id": 23}, { $unset: {"user.tasks.$.due_on": ""}});
 result = db.projects.find({projectID:124});
-print('Result after Updating the task fields once it is completed');
+print('Result after Updating the task fields - Removing Due on and adding completed status');
 while(result.hasNext()){
 printjson(result.next());
 }
 
 //Sorting the username of the members of a project in alphabetical order 
-db.projects.update({"project": "P1"}, {$push: {"user.userName": {$each: [], $sort:1}}});
-result = db.projects.find({"project": "P1"});
-print('Result after Updating the task fields once it is completed');
+db.projects.update({project: "P1"}, {$push: {user: {$sort: {userName:1}}}});
+result = db.projects.find({project: "P1"});
+print('Result after Sorting the username of the members of a project in alphabetical order ');
 while(result.hasNext()){
 printjson(result.next());
 }
 
 //Removing the first user from the sorted user list
 db.projects.update({"project": "P1"}, {$pop: {"user": {key: -1}}});
-result = db.projects.find({"project": "P1"});
-print('Result after Updating the task fields once it is completed');
+result = db.projects.find({project: "P1"});
+print('Result after Removing the first user from the sorted user list');
 while(result.hasNext()){
 printjson(result.next());
 }
@@ -28,8 +28,8 @@ printjson(result.next());
 
 //Upserting sections to a project
 db.projects.update({"project": "P2"}, {$setOnInsert: {"section.sectionID": "100"}}, true);
-result = db.projects.find({"project": "P2"});
-print('Result after Updating the task fields once it is completed');
+result = db.projects.find({project: "P2"});
+print('Result after Upserting sections to a project');
 while(result.hasNext()){
 printjson(result.next());
 }
